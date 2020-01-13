9e1686db4fec1c2b096b5b877939bb9281fc91f4
my3tahk <pleh@ya.ru>
2020-01-14 00:33:24 +0300

    1st homework

---
+++ hw01-maven/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus;
+
+import static com.google.common.primitives.UnsignedInts.divide;
+
+public class HelloOtus {
+    public static int callGuavaMethod() {
+        return divide(20, 5);
+    }
+}
